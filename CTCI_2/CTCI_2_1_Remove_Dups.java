bec9640d351d5fc5d34a96e1606262788163a6c3
kallol <kalloldash@gmail.com>
2018-06-23 16:58:40 -0600

    Solved ctci 2.1

---
+++ CTCI_2/CTCI_2_1_Remove_Dups.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CTCI_2;
+
+/**
+ *
+ * @author KD
+ * 
+ * cracking the coding interview 2.1
+ * 
+ * write code to remove duplicates from an unsorted linked list
+ */
+
+import java.util.*;
+public class CTCI_2_1_Remove_Dups {
+    
+    public ListNode delDuplicates(ListNode n){
+        HashSet<Integer> hashSet = new HashSet<Integer>();
+        ListNode head = n;
+        ListNode prev = null;
+        
+        while(n != null) {
+            if (hashSet.contains(n.val)){
+                prev.next = n.next;
+            }
+            else{
+                hashSet.add(n.val);
+                prev = n;
+            }
+            n = n.next;
+        }
+        return head;
+    }
+}
+
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
