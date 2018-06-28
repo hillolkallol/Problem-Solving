5fd758fb43cd9710d9d8d7bcf77afde9bce8760c
kallol <kalloldash@gmail.com>
2018-06-27 19:48:04 -0600

    Solved ctci 2.8

---
+++ CTCI_2_8_Loop_Detection.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author KD
+ * 
+ * Cracking the coding interview 2.8
+ * 
+ * 2.8 Loop Detection: Given a circular linked list, implement an algorithm that returns the node at the
+beginning of the loop.
+DEFINITION
+Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, so
+as to make a loop in the linked list.
+EXAMPLE
+Input: A -> B -> C - > D -> E -> C [the same C as earlier)
+Output: C
+ * 
+ * 
+ */
+public class CTCI_2_8_Loop_Detection {
+    public boolean detectLoop(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+}
