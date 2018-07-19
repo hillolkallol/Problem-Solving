210def9bd0e2c59958378e885f412ad0fef578ce
kallol <kalloldash@gmail.com>
2018-07-19 00:30:29 -0600

    leetcode 20 in java

---
+++ Leetcode_20/Leetcode_20_Valid_Parentheses.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Leetcode_20;
+
+/**
+ *
+ * @author KD
+ * 
+ * 20. Valid Parentheses
+ * 
+ * 
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+Example 3:
+
+Input: "(]"
+Output: false
+Example 4:
+
+Input: "([)]"
+Output: false
+Example 5:
+
+Input: "{[]}"
+Output: true
+ */
+import java.util.*;
+public class Leetcode_20_Valid_Parentheses {
+    
+}
+class Solution{
+    public boolean isValid(String s){
+        Stack<Character> st = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c == '(') st.push(')');
+            else if(c == '{') st.push('}');
+            else if(c == '[') st.push(']');
+            else{
+                if (st.empty()) return false;
+                if (c != st.pop()) return false;
+            }
+        }
+        return st.empty();
+    }
+}
