60443b56d8a8e2f51789dc479b20ed4c99ddffe6
kallol <kalloldash@gmail.com>
2018-08-14 17:08:28 -0600

    Solved leetcode 746

---
+++ Leetcode_746_MinCostClimbingStairs.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author KD
+ * 
+ * 746. Min Cost Climbing Stairs
+On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+
+Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.
+
+Example 1:
+Input: cost = [10, 15, 20]
+Output: 15
+Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+Example 2:
+Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+Output: 6
+Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
+Note:
+cost will have a length in the range [2, 1000].
+Every cost[i] will be an integer in the range [0, 999].
+ */
+public class Leetcode_746_MinCostClimbingStairs {
+    public int minCostClimbingStairs(int[] cost) {
+        int a = cost[0];
+        int b = cost[1];
+        int temp;
+        
+        for (int i=2; i < cost.length; i++) {
+            temp = Math.min(cost[i] + a, cost[i] + b);
+            a = b;
+            b = temp;
+        }
+        return Math.min(a, b);
+    }
+}
