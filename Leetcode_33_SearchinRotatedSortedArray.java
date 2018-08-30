a68687ee850b21ec1297f3e0e1964fbe6ece27ee
kallol <kalloldash@gmail.com>
2018-08-29 23:12:47 -0600

    Solved leetcode 33

---
+++ Leetcode_33_SearchinRotatedSortedArray.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author KD
+ * 
+ * 33. Search in Rotated Sorted Array
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+ */
+public class Leetcode_33_SearchinRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            int num = nums[mid];
+            if ((nums[mid] < nums[0]) != (target < nums[0]))
+                num = target < nums[0] ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+
+            if (num < target) low = mid + 1;
+            else if (num > target) high = mid - 1;
+            else return mid;
+        }
+        return -1;
+    }
+}
