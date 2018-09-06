f084c236db903164edbeb71a642b9a2c263c8cdb
kallol <kalloldash@gmail.com>
2018-09-06 17:25:07 -0600

    Solved Leetcode 50

---
+++ Leetcode_50_Pow_x_n.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author KD
+ * 
+ * 
+ * 50. Pow(x, n)
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+Example 3:
+
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+Note:
+
+-100.0 < x < 100.0
+n is a 32-bit signed integer, within the range [−231, 231 − 1]
+ * 
+ */
+public class Leetcode_50_Pow_x_n {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1/x;
+            N = -N;
+        }
+        
+        double ans = 1;
+        double current_x = x;
+        for (long i = N; i > 0; i /= 2) {
+            if (i % 2 == 1) ans *= current_x;
+            current_x *= current_x;
+        }
+        
+        return ans;
+    }
+}
