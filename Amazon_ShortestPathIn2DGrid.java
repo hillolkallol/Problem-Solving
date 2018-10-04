8804b35111514e878d49ae7fdce52ee6f74cb18b
kallol <kalloldash@gmail.com>
2018-10-04 16:57:35 -0600

    Solved Amazon interview question- finding shortest path in a 2d grid

--- Amazon_ShortestPathIn2DGrid.java
+++ Amazon_ShortestPathIn2DGrid.java
@@ -7,6 +7,20 @@
 /**
  *
  * @author KD
+ * 
+ * Given a 2 dimensional matrix where some of the elements are filled with 1 and rest of the elements 
+are filled. Here X means you cannot traverse to that particular points. From a cell you can either traverse to left, right, up or down 
+Given two points in the matrix find the shortest path 
+between these points 
+
+For example if the matrix is 
+1 1 1 1 1 
+S 1 X 1 1 
+1 1 1 1 1 
+X 1 1 E 1 
+1 1 1 1 X 
+
+Here S is the starting point and E is the Ending point
  */
 import java.util.*;
 public class Amazon_ShortestPathIn2DGrid {
