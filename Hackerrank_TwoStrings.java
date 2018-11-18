1923e54b8c22a259b8d4b1b3d49b799dc1a293bf
kallol <kalloldash@gmail.com>
2018-11-18 00:57:59 -0700

    Solved hackerrank | TwoStrings

---
+++ Hackerrank_TwoStrings.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author KD
+ * 
+ * Two Strings
+ * 
+ * Given two strings, determine if they share a common substring. A substring may be as small as one character.
+
+For example, the words "a", "and", "art" share the common substring . The words "be" and "cat" do not share a substring.
+
+Function Description
+
+Complete the function twoStrings in the editor below. It should return a string, either YES or NO based on whether the strings share a common substring.
+
+twoStrings has the following parameter(s):
+
+s1, s2: two strings to analyze .
+Input Format
+
+The first line contains a single integer , the number of test cases.
+
+The following  pairs of lines are as follows:
+
+The first line contains string .
+The second line contains string .
+Constraints
+
+ and  consist of characters in the range ascii[a-z].
+Output Format
+
+For each pair of strings, return YES or NO.
+
+Sample Input
+
+2
+hello
+world
+hi
+world
+Sample Output
+
+YES
+NO
+Explanation
+
+We have  pairs to check:
+
+, . The substrings  and  are common to both strings.
+, .  and  share no common substrings.
+ */
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Hackerrank_TwoStrings {
+    
+}
+
+class Solutionkid {
+
+    // Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+        char[] arr = new char[26];
+
+        for (char c: s1.toCharArray()) arr[c-'a']++;
+        for (char c: s2.toCharArray())
+            if (arr[c-'a'] > 0) return "YES";
+
+        return "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
