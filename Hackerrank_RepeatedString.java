c89c1ea1eafc32118bb4f6b990c6b9b920f9dd40
kallol <kalloldash@gmail.com>
2018-11-18 22:19:45 -0700

    Solved hackerrank Repeated String

---
+++ Hackerrank_RepeatedString.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author KD
+ * 
+ * Repeated String
+ * 
+ * Lilah has a string, , of lowercase English letters that she repeated infinitely many times.
+
+Given an integer, , find and print the number of letter a's in the first  letters of Lilah's infinite string.
+
+For example, if the string  and , the substring we consider is , the first  characters of her infinite string. There are  occurrences of a in the substring.
+
+Function Description
+
+Complete the repeatedString function in the editor below. It should return an integer representing the number of occurrences of a in the prefix of length  in the infinitely repeating string.
+
+repeatedString has the following parameter(s):
+
+s: a string to repeat
+n: the number of characters to consider
+Input Format
+
+The first line contains a single string, . 
+The second line contains an integer, .
+
+Constraints
+
+For  of the test cases, .
+Output Format
+
+Print a single integer denoting the number of letter a's in the first  letters of the infinite string created by repeating  infinitely many times.
+
+Sample Input 0
+
+aba
+10
+Sample Output 0
+
+7
+Explanation 0 
+The first  letters of the infinite string are abaabaabaa. Because there are  a's, we print  on a new line.
+
+Sample Input 1
+
+a
+1000000000000
+Sample Output 1
+
+1000000000000
+Explanation 1 
+Because all of the first  letters of the infinite string are a, we print  on a new line.
+ */
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Hackerrank_RepeatedString {
+    
+}
+
+class Solutioaassn {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+        int counta = countA(s);
+        
+        int slen = s.length();
+        long rep = n / slen;
+        long ext = n % slen;
+
+        return counta * rep + countA(s.substring(0, (int)ext));
+    }
+
+    public static int countA(String s) {
+        int counta = 0;
+        for (char c : s.toCharArray()) {
+            if (c == 'a')
+                counta++;
+        }
+        return counta;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        long n = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
